741a84674405a8dd2bed6843c8d734db9a7f020c
Victor Hernandez <vhugohh@hotmail.com>
2015-05-04 12:59:52 -0500

    Detect Face, Control Car with Network (WiFi) and Bluetooth and Arduino

---
+++ app/src/main/java/networkserver/NetSettings.java
@@ -0,0 +1,17 @@
+package networkserver;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.qualcomm.snapdragon.sdk.recognition.sample.R;
+
+public class NetSettings extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.network_settings);
+
+
+
+    }
+
+}
